19e5d132c6c6881b017935b833bb2388e1825d5f
staristic <dnadnaacgt@gmail.com>
2017-09-09 22:59:22 +0800

    radar figure in simple table

--- src/components/SimpleComparisonTable.vue
+++ src/components/SimpleComparisonTable.vue
@@ -9,7 +9,7 @@
       </tr>
       <tr v-for="(bill, index) in model.bills">
         <td>{{bill.name}}</td>
-        <td>radar figure here</td>
+        <td><div v-bind:id="'radar' + index" v-bind:ref="'radar' + index"></div></td>
         <td v-for="act in bill.act_features">{{act.short_content}}</td>
       </tr>
     </table>
@@ -18,29 +18,71 @@
 </template>
 
 <script>
+import * as radar from '@/util/radar'
+
+const getMaxScore = function (array) {
+  var max = 0
+  for (let i of array) {
+    if (i.score) {
+      max = max >= i.score ? max : i.score
+    }
+  }
+  return max * 1.02
+}
+
 export default {
-  props: ['value'],
+  props: ['value', 'parentInitialized'],
   data () {
     return {
-      model: this.value,
-      max: 0,
-      score: []
+      mounted: false
     }
   },
-  beforeMount () {
-    this.model = this.value
+  computed: {
+    model () {
+      return this.value
+    }
   },
   watch: {
-    'value' () {
-      this.pull()
+    'parentInitialized' () {
+      this.drawAll()
+    },
+    'mounted' () {
+      this.drawAll()
     }
   },
   mounted () {
-    this.pull()
+    this.mounted = true
   },
   methods: {
-    pull () {
-      this.model = this.value
+    drawAll () {
+      for (let i = 0; i < this.model.bills.length; i++) {
+        this.$nextTick(() => {
+          this.draw(i)
+        })
+      }
+    },
+    draw (billIndex) {
+      if (this.parentInitialized && this.mounted && billIndex < this.model.bills.length) {
+        const bill = this.model.bills[billIndex]
+        var radarInfo = []
+        var content = []
+        for (let i of bill.act_features) {
+          content.push({
+            feature: i.act_feature_title,
+            score: i.score
+          })
+        }
+        radarInfo.push(content)
+        var maxValue = getMaxScore(content)
+        var options = {
+          w: 150,
+          h: 150,
+          maxValue: maxValue,
+          levels: 5,
+          segments: false
+        }
+        radar.draw(this.$el.querySelector('#radar' + billIndex), radarInfo, options, this.$el.querySelector('#radar' + billIndex))
+      }
     }
   }
 }
