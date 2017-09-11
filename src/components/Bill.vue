0e8a025014131f3442166c72132879070380ecb5
Wesley <wesley100002001@gmail.com>
2017-09-11 13:29:39 +0800

    Revise wording and introductions in Creation page

--- src/components/Bill.vue
+++ src/components/Bill.vue
@@ -2,27 +2,27 @@
   <div class="report-create">
     <h1>👩‍🔬👨‍🔬</h1>
     <el-form ref="form" :model="form">
-      <el-form-item label="Topic">
-        <el-input placeholder="Topic" v-model="form.topic"></el-input>
+      <h3>Topic</h3>
+      <el-form-item label="A public topic related to certain act">
+        <el-input placeholder="Eg. Marriage Equality" v-model="form.topic"></el-input>
       </el-form-item>
-      <el-form-item label="Act">
-        <el-input placeholder="Act" v-model="form.act"></el-input>
-      </el-form-item>
-      <el-form-item label="Act Features">
+      <h3>Features</h3>
+      <el-form-item label="Features that use to compare between acts">
       </el-form-item>
       <el-form-item v-for="af in form.actFeatures" :key="index">
-        <el-input placeholder="Lower vote threshold" v-model="af.feature"></el-input>
+        <el-input placeholder="Eg: Allowing adoption?" v-model="af.feature"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="text" icon="plus" @click="addActFeature">Add act feature</el-button>
+        <el-button type="text" icon="plus" @click="addActFeature">Add more feature</el-button>
       </el-form-item>
-      <el-form-item label="Bill">
+      <h3>Acts to be compare</h3>
+      <el-form-item label="Specific acts in different country">
       </el-form-item>
       <el-form-item v-for="(bill, index) in form.bills" :key="index">
-        <el-form-item :label="'Bill ' + (index + 1)"></el-form-item>
-        <el-form-item label="Bill Name">
-          <el-input placeholder="Bill Name" v-model="bill.name"></el-input>
+        <el-form-item label="Act in Country">
+          <el-input placeholder="Eg: Act of marriage equality in Taiwan" v-model="bill.name"></el-input>
         </el-form-item>
+        <h5>Features of act</h5>
         <el-col :span="8">Short Content</el-col>
         <el-col :span="1"><div class="grid-content"></div></el-col>
         <el-col :span="8">Content</el-col>
@@ -31,20 +31,22 @@
         <el-form-item v-for="baf in bill.act_features" :key="index">
           <el-form-item :label="baf.act_feature_title"></el-form-item>
           <el-col :span="8">
-            <el-input v-model="baf.short_content"></el-input>
+            <el-input placeholder="Allowed" v-model="baf.short_content"></el-input>
           </el-col>
           <el-col :span="1"><div class="grid-content"></div></el-col>
           <el-col :span="8">
-            <el-input v-model="baf.content"></el-input>
+            <el-input placeholder="Adoption is allowed for any legal couple" type="textarea" v-model="baf.content"></el-input>
           </el-col>
           <el-col :span="1"><div class="grid-content"></div></el-col>
           <el-col :span="5">
-            <el-input-number v-model="baf.score" :min="1" :max="5"></el-input-number>
+            <el-tooltip class="item" effect="light" content="Score this feature from 1 to 5" placement="top-start">
+              <el-input-number v-model="baf.score" :min="1" :max="5"></el-input-number>
+            </el-tooltip>
           </el-col>
         </el-form-item>
       </el-form-item>
       <el-form-item>
-        <el-button type="text" icon="plus" @click="addBill">Add bill</el-button>
+        <el-button type="text" icon="plus" @click="addBill">Add more act</el-button>
       </el-form-item>
       <el-form-item>
         <el-button v-if="isNew" type="primary" size="large" @click="submit">Create a dataReport</el-button>
@@ -93,6 +95,10 @@ export default {
       })
     },
     addBill () {
+      if (this.form.actFeatures.length < 3) {
+        alert('Oops! At least 3 features are needed.')
+        return
+      }
       var tmpActFeature = []
       this.form.actFeatures.forEach(item => {
         tmpActFeature.push({
